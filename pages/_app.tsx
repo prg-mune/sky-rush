d13aeab9a23fae242f8b0fd30f7cf3e277989227
prg-mune <prg-mune@users.noreply.github.com>
2026-06-25 09:20:28 +0900

    Add Sky Rush favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
